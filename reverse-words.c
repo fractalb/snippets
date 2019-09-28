c8985cc106d14376718bf9448d77c7f7c50bb74a
Balaji Marisetti <balajimarisetti@gmail.com>
2019-09-28 13:45:45 +0530

    Reverse words

---
+++ reverse-words.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+/* Words delimeted by spaces. */
+ 
+static inline void swap(char *start, char *end)
+{
+	char temp = *start;
+	*start = *end;
+	*end = temp;
+
+	return;
+}
+
+void reverse_chars(char *start, char *end)
+{
+	while (end > start) {
+		swap(start, end);
+		start++, end--;
+	}
+
+	return;
+}
+
+void reverse_words(char *str)
+{
+	char *start, *end;
+	start = end = str;
+
+	while (*end != '\0') {
+		end++;
+		if (*end == ' ' || *end == '\t' || *end == '\n') {
+			reverse_chars(start, end-1);
+			end++;
+			start = end;
+		}
+	}
+
+	end--;
+	reverse_chars(start, end);
+	reverse_chars(str, end);
+	return;
+}
+
+int main()
+{
+	char input_string[8] = { 0 };
+	printf("Enter a string: ");
+	fgets(input_string, sizeof(input_string), stdin);
+
+	reverse_words(input_string);
+
+	printf("Reversed words: %s\n", input_string);
+	return 0;
+}
