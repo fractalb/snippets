a9f630b3c513ecf0d4492af84a966280b0034e30
Balaji Marisetti <balajimarisetti@gmail.com>
2019-09-28 22:08:33 +0530

    Added code to correctly handle spaces at the begining and the ending of the string

--- reverse-words.c
+++ reverse-words.c
@@ -1,13 +1,11 @@
+/* Reverse words delimeted by spaces or tabs */
 #include <stdio.h>
 
-/* Words delimeted by spaces. */
- 
 static inline void swap(char *start, char *end)
 {
 	char temp = *start;
 	*start = *end;
 	*end = temp;
-
 	return;
 }
 
@@ -17,20 +15,33 @@ void reverse_chars(char *start, char *end)
 		swap(start, end);
 		start++, end--;
 	}
-
 	return;
 }
 
+/*
+ * Only spaces or tabs are treated as word delimeters.
+ * Everything else is treated as part of a word.
+ * The str argument is a NUL terminated C-string.
+ */
 void reverse_words(char *str)
 {
 	char *start, *end;
-	start = end = str;
+	start = str;
 
+	/* make sure that 'start' points to a
+         * non-space character */
+	while (*start == ' ' || *start == '\t')
+		start++;
+
+	end = start;
 	while (*end != '\0') {
 		end++;
-		if (*end == ' ' || *end == '\t' || *end == '\n') {
-			reverse_chars(start, end-1);
+		if (*end == ' ' || *end == '\t') {
+			reverse_chars(start, end - 1);
 			end++;
+			/* Skip to next non-space character */
+			while (*end == ' ' || *end == '\t')
+				end++;
 			start = end;
 		}
 	}
@@ -46,9 +57,8 @@ int main()
 {
 	char input_string[128];
 
-	printf("Enter a string: ");
 	fgets(input_string, sizeof(input_string), stdin);
 	reverse_words(input_string);
-	printf("Reversed words: %s\n", input_string);
+	printf("%s\n", input_string);
 	return 0;
 }
