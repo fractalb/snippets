e53838d43fdd83ff2adc35fdb8ae5d35f13f689b
Balaji Marisetti <balajimarisetti@gmail.com>
2023-04-21 15:03:23 +0530

    Added hex2binary-cmd program

--- Makefile
+++ Makefile
@@ -1,5 +1,16 @@
 
-all: hex2binary-test
+all: hex2binary-test hex2binary-cmd hex-dump
+
+hex-dump: hex-dump.c
+	gcc -std=gnu11 -Wall -Wextra $< -o $@
 
 hex2binary-test: hex2binary.c unittest_hex2binary.cc
-	g++ unittest_hex2binary.cc -lgtest -lgtest_main -o $@
+	g++ -Wall unittest_hex2binary.cc -lgtest -lgtest_main -o $@
+
+hex2binary-cmd: hex2binary-cmd.cc hex2binary.c
+	g++ -std=c++17 -Wall -Wextra $< -o $@
+
+.PHONY=clean
+clean:
+	rm -f *.o
+	rm -f hex-dump hex2binary-test hex2binary-cmd
---
+++ hex2binary-cmd.cc
@@ -0,0 +1,87 @@
+#include <fstream>
+#include <iostream>
+#include <stdexcept>
+
+extern "C" {
+#include "hex2binary.c"
+}
+
+class Buffer {
+private:
+  char *buf;
+  size_t size;
+  static const size_t init_size;
+
+public:
+  Buffer();
+  Buffer(size_t size);
+  ~Buffer();
+
+  Buffer &operator=(const Buffer &b) = delete;
+  void resizeBuf(size_t newSize);
+  void ensureBufSize(size_t size);
+  inline char *getBuf() { return buf; }
+  inline size_t getSize() { return size; }
+};
+
+const size_t Buffer::init_size = 32;
+Buffer::Buffer() : Buffer(init_size) {}
+
+Buffer::Buffer(size_t size) : buf(nullptr), size(size) {
+  buf = (char *)malloc(size);
+  if (buf == nullptr) {
+    throw std::bad_alloc();
+  }
+}
+
+Buffer::~Buffer() {
+  free(buf);
+  size = 0;
+}
+
+void Buffer::resizeBuf(size_t newSize) {
+  char *buf2 = (char *)malloc(newSize);
+  if (buf2 == nullptr)
+    throw std::bad_alloc();
+  free(buf);
+  buf = buf2;
+  size = newSize;
+}
+
+void Buffer::ensureBufSize(size_t size) {
+  if (size <= this->size)
+    return;
+
+  resizeBuf(size);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    std::cerr << "File name is needed\n";
+    return 0;
+  }
+  std::ifstream ifs(argv[1], std::ifstream::in);
+  if (!ifs) {
+    std::cerr << "Error opening file " << argv[1] << '\n';
+    return EINVAL;
+  }
+  int error;
+  Buffer buf;
+  std::string line;
+  std::getline(ifs, line);
+  while (ifs) {
+    buf.ensureBufSize(line.size());
+    size_t s = decode_hex_to_binary(line.c_str(), line.size(), buf.getBuf(),
+                                    buf.getSize(), &error);
+    std::cerr << "decoded bytes s = " << s << "\n";
+    if (error == -1) {
+      std::cerr << "Error decoding hex string: " << line << '\n';
+    }
+    for (size_t i = 0; i < s; ++i) {
+      std::cout << buf.getBuf()[i];
+    }
+    std::cout << '\n';
+    std::getline(ifs, line);
+  }
+  return 0;
+}
